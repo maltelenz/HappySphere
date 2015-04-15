08a9480ebb803b342537031fc1e84f4ab348d3e8
Malte Lenz <malte.lenz@gmail.com>
2015-04-15 21:00:18 +0200

    Refactor level 2 into more general form.

--- src/com/maltelenz/sensortrouble/Level2Screen.java
+++ src/com/maltelenz/sensortrouble/Level2Screen.java
@@ -1,80 +1,18 @@
 package com.maltelenz.sensortrouble;
 
-import java.util.List;
-
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Paint.Style;
-import android.graphics.Typeface;
-
 import com.maltelenz.framework.Game;
-import com.maltelenz.framework.Graphics;
-import com.maltelenz.framework.Input.TouchEvent;
 
-public class Level2Screen extends LevelScreen {
-
-    float touchLength = 500;
-    float timeLeft = touchLength;
-    
-    int touching = 0;
-    
-    int circleRadius;
+public class Level2Screen extends ContinousTouchButtonLevel {
 
     public Level2Screen(Game game) {
         super(game);
 
+        maxTouchTime = 500;
+
         circleRadius = game.scale(300);
+
+        circles.add(new ContinuousCountdownButton(circleRadius, gameWidth/2, gameHeight/2, maxTouchTime));
 
         state = GameState.Running;
     }
-
-    @Override
-    protected void updateGameRunning(List<TouchEvent> touchEvents, float deltaTime) {
-        
-        if (touching > 0) {
-            timeLeft -= deltaTime * touching;
-        }
-        
-        int len = touchEvents.size();
-        for (int i = 0; i < len; i++) {
-            TouchEvent event = touchEvents.get(i);
-
-            if (event.type == TouchEvent.TOUCH_DOWN) {
-                touching++;
-            }
-            if (event.type == TouchEvent.TOUCH_UP) {
-                touching--;
-            }
-        }
-        if (timeLeft < 0) {
-            state = GameState.Finished;
-        }
-    }
-
-    @Override
-    double percentDone() {
-        return (touchLength - timeLeft)/touchLength;
-    }
-
-    void drawRunningUI() {
-        Graphics g = game.getGraphics();
-        g.clearScreen(ColorPalette.background);
-
-        Paint circlePainter = new Paint();
-        circlePainter.setColor(ColorPalette.button);
-        circlePainter.setStyle(Style.FILL_AND_STROKE);
-        circlePainter.setStrokeWidth(game.scale(5));
-        circlePainter.setAntiAlias(true);
-        circlePainter.setShadowLayer(game.scale(10.0f), game.scale(2.0f), game.scale(2.0f), ColorPalette.buttonShadow);
-        g.drawCircle(g.getWidth()/2, g.getHeight()/2, circleRadius, circlePainter);
-
-        Paint largePainter = new Paint();
-        largePainter.setTextSize(game.scale(150));
-        largePainter.setTextAlign(Paint.Align.CENTER);
-        largePainter.setAntiAlias(true);
-        largePainter.setColor(Color.WHITE);
-        largePainter.setTypeface(Typeface.DEFAULT_BOLD);
-        g.drawStringCentered(String.format("%.1f", timeLeft), largePainter);
-    }
-
 }
