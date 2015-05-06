dc0d930bb501f70467bed7a4324f4662ac2405fd
Malte Lenz <malte.lenz@gmail.com>
2015-05-06 22:05:50 +0200

    Adapt happier color scheme + new icon.

--- src/com/maltelenz/sensortrouble/ColorPalette.java
+++ src/com/maltelenz/sensortrouble/ColorPalette.java
@@ -4,7 +4,7 @@ import android.graphics.Color;
 
 public class ColorPalette {
     public static int background = Color.rgb(196, 246, 255);
-    public static int button = Color.rgb(0, 132, 156);
+    public static int button = Color.rgb(11, 176, 222);
 
     public static int progress = Color.rgb(169, 222, 51);
     public static int inactiveProgress = Color.argb(100, 169, 222, 51);
@@ -18,7 +18,7 @@ public class ColorPalette {
 
     public static int box = Color.rgb(70, 70, 70);
 
-    public static int laser = Color.rgb(232, 110, 35);
+    public static int laser = Color.rgb(245, 166, 39);
 
     public static int oopsie = Color.rgb(214, 128, 36);
 
